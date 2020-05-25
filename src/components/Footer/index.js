6dd0cfaa9b618f33f2cd10bde8591cd07f45c7f9
SivaramPg <sivarampg95@gmail.com>
2020-05-25 19:20:51 +0530

    Footer content and styling

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,102 @@ import styled from 'styled-components';
 const Footer = () => {
 	return (
 		<FooterWrapper>
-			<h1>Footer</h1>
+			<FooterColumn>
+				<p>
+					Made with{' '}
+					<span
+						style={{
+							color: 'tomato',
+						}}
+					>
+						&hearts;
+					</span>{' '}
+					by{' '}
+					<a
+						style={{
+							textDecoration: 'none',
+							fontWeight: 'bold',
+							color: 'darkorchid',
+						}}
+						href="https://sivarampg.com"
+					>
+						Sivaram Pandariganthan
+					</a>
+				</p>
+			</FooterColumn>
+			<FooterColumn>
+				<p
+					style={{
+						color: 'tomato',
+					}}
+				>
+					Built Using:
+				</p>
+				<TechLink>
+					<img src="https://reactjs.org/favicon.ico" alt="reactjs.org" />
+					&nbsp;
+					<a
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						React
+					</a>
+				</TechLink>
+				<TechLink>
+					<img
+						src="https://styled-components.com/favicon.png"
+						alt="styled-components.com"
+					/>
+					&nbsp;
+					<a
+						href="https://styled-components.com"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Styled Components
+					</a>
+				</TechLink>
+			</FooterColumn>
+			<FooterColumn>
+				<p
+					style={{
+						color: 'tomato',
+					}}
+				>
+					Data Fetched From:
+				</p>
+				<TechLink>
+					<img src="https://swapi.dev/static/favicon.ico" alt="swapi.dev" />
+					&nbsp;
+					<a
+						href="https://swapi.dev/"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						swapi.dev
+					</a>
+				</TechLink>
+			</FooterColumn>
+			<FooterColumn>
+				<TechLink>
+					<img
+						style={{
+							backgroundColor: '#fff',
+						}}
+						src="https://github.githubassets.com/pinned-octocat.svg"
+						alt="github.com"
+					/>
+					&nbsp;
+					<a
+						href="https://github.com/SivaramPg/swapi-deux"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						View on Github
+					</a>
+				</TechLink>
+			</FooterColumn>
 		</FooterWrapper>
 	);
 };
@@ -15,7 +110,43 @@ const FooterWrapper = styled.div`
 	width: 100vw;
 	height: 100px;
 
+	display: flex;
+	justify-content: space-evenly;
+	align-items: center;
+
 	color: #fff;
 
 	background-color: black;
+`;
+
+const FooterColumn = styled.div`
+	flex: 0 1 25%;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	flex-flow: column nowrap;
+`;
+
+const TechLink = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	flex-flow: row nowrap;
+
+	margin: 10px 0 0 0;
+
+	& img {
+		width: 20px;
+		height: 20px;
+		border-radius: 50%;
+	}
+
+	& a {
+		font-size: 14px;
+		color: white;
+		text-decoration: none;
+	}
 `;
