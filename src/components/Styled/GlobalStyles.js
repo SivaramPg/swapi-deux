0529de39003a9554f3baf126317bccf127d3b07c
SivaramPg <sivarampg95@gmail.com>
2020-05-25 16:51:10 +0530

    global custom scrollbar

--- src/components/Styled/GlobalStyles.js
+++ src/components/Styled/GlobalStyles.js
@@ -11,11 +11,33 @@ const GlobalStyle = createGlobalStyle`
 
     body {
         box-sizing: border-box;
+        overflow-x: hidden;
         font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
             'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
             sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
+
+            		/* width */
+		::-webkit-scrollbar {
+			width: 5px;
+		}
+
+		/* Track */
+		::-webkit-scrollbar-track {
+			background: #f1f1f1;
+		}
+
+		/* Handle */
+		::-webkit-scrollbar-thumb {
+			background: #888;
+			border-radius: 2.5px;
+		}
+
+		/* Handle on hover */
+		::-webkit-scrollbar-thumb:hover {
+			background: #555;
+		}
     }
 `;
 
