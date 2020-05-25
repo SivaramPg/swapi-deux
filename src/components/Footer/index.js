abc3155ec5e15ef4ade592d6b388e1c1d4711566
SivaramPg <sivarampg95@gmail.com>
2020-05-25 16:50:41 +0530

    Footer wrapper styled component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,10 +3,19 @@ import styled from 'styled-components';
 
 const Footer = () => {
 	return (
-		<div>
+		<FooterWrapper>
 			<h1>Footer</h1>
-		</div>
+		</FooterWrapper>
 	);
 };
 
 export default Footer;
+
+const FooterWrapper = styled.div`
+	width: 100vw;
+	height: 100px;
+
+	color: #fff;
+
+	background-color: black;
+`;
