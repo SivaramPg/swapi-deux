9ab196b0d149343502c5b3ad7712e4636d5abc94
SivaramPg <sivarampg95@gmail.com>
2020-05-25 14:47:45 +0530

    Search box componenet

---
+++ src/components/SearchBox/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SearchBox = ({ searchText, onSearch }) => {
+	return (
+		<SearchInput
+			type="text"
+			onChange={onSearch}
+			value={searchText}
+			placeholder="Search for a Star Wars character"
+		/>
+	);
+};
+
+export default SearchBox;
+
+const SearchInput = styled.input`
+	@media only screen and (min-width: 768px) {
+		width: 400px;
+		height: 50px;
+
+		border-radius: 5px;
+		padding: 0 20px;
+		font-size: 24px;
+	}
+`;
